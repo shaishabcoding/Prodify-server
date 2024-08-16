980ed3c53a8bca03b6848b88ca99719ecf1ce5ca
shaishabcoding <myshaishab@gmail.com>
2024-08-16 19:02:58 +0600

    added paginaton

--- index.js
+++ index.js
@@ -34,8 +34,45 @@ const client = new MongoClient(process.env.DB_URI, {
   });
 
   app.get("/products", async (req, res) => {
-    const products = await productCollection.find().toArray();
-    const productsCount = await productCollection.countDocuments();
+    const limit = parseInt(req.query.limit, 10) || 10;
+    const offset = parseInt(req.query.offset, 10) || 0;
+    const { search, sort } = req.query;
+    const matchStage = {};
+
+    if (search) {
+      matchStage.$text = { $search: search };
+    }
+
+    // Define sorting logic based on query parameters
+    let sortStage = {};
+    if (sort === "priceLowToHigh") {
+      sortStage = { price: 1 }; // Ascending order
+    } else if (sort === "priceHighToLow") {
+      sortStage = { price: -1 }; // Descending order
+    } else if (sort === "dateNewest") {
+      sortStage = { productCreationDate: -1 }; // Newest first
+    } else {
+      sortStage = search ? { score: { $meta: "textScore" } } : { _id: 1 }; // Default sort
+    }
+
+    const products = await productCollection
+      .aggregate([
+        {
+          $match: matchStage,
+        },
+        {
+          $sort: sortStage,
+        },
+        {
+          $skip: offset,
+        },
+        {
+          $limit: limit,
+        },
+      ])
+      .toArray();
+
+    const productsCount = await productCollection.countDocuments(matchStage);
     res.send({ products, productsCount });
   });
 })();
